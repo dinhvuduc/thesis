392b4a2ed96460451170ceae2ce729a9a8342743
dinhvuduc <dinhvuduc751@gmail.com>
2024-02-26 04:15:05 +0700

    update select landing page

--- src/app/Pages/landing/landing.component.ts
+++ src/app/Pages/landing/landing.component.ts
@@ -1,17 +1,52 @@
 import { Component } from '@angular/core';
 import { StepComponent } from '../../components/step/step.component';
 import { ButtonComponent } from '../../components/button/button.component';
+import { SelectComponent } from '../../components/select/select.component';
+type WorkoutType = 'broSplit' | 'individual' | 'upperLower' | 'bodyBuilder'
 
+type WorkoutGoal = 'strength' | 'cardiovascular' | 'growth'
+
+const MUSCLE_GROUPS: Record<WorkoutType, string[]> = {
+  individual: ['biceps', 'triceps', 'chest'],
+  broSplit: ['push', 'pull', 'legs'],
+  upperLower: ['upper', 'lower'],
+  bodyBuilder: ['chest', 'back', 'shoulder'],
+};
 @Component({
   selector: 'app-landing-page',
   standalone: true,
   imports: [
     StepComponent,
-    ButtonComponent
+    ButtonComponent,
+    SelectComponent
   ],
   templateUrl: './landing.component.html',
   styleUrl: './landing.component.scss'
 })
 export class LandingPageComponent {
+  selectedWorkoutType: WorkoutType | undefined ; 
+  selectedGoal: WorkoutGoal | undefined;
+
+  onClick(workoutType: WorkoutType) {
+    this.selectedWorkoutType = workoutType;
+  }
+
+  checkSelectedWorkoutType(workoutType:WorkoutType){
+    return this.selectedWorkoutType === workoutType ? 'transparent' : 'black';
+  }
+
+  get muscleGroups() {
+    if (this.selectedWorkoutType === undefined) return [];
+    return MUSCLE_GROUPS[this.selectedWorkoutType];
+  }
+
+  onselectgoal(goal: WorkoutGoal){
+    this.selectedGoal = goal;
+
+  }
+
+  checkSelectedGoal(goal:WorkoutGoal){
+    return this.selectedGoal === goal ? 'transparent' : 'black';
+  }
 
 }
---
+++ src/app/components/select/select.component.ts
@@ -0,0 +1,19 @@
+
+import { NgFor } from '@angular/common';
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-select',
+  standalone: true,
+  imports: [
+    NgFor, FormsModule
+  ],
+  templateUrl: './select.component.html',
+  styleUrl: './select.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class SelectComponent { 
+  @Input() options: string[] = [];
+  value="da"
+}
